2b4941cf8a4763d9fc1d23d18b080dd61051d18e
mily datta <dmily270@gmail.com>
2021-08-15 22:18:11 +0600

    some update

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,10 @@
-
-function App () {
+function App() {
   return (
     <div>
       <h1>hello buy n save</h1>
       <p>lorem ipsum dolor sit</p>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
